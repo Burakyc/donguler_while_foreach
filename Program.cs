85bcd0fc924294269d8dc5806c8d2d19b11f0b08
Burak <115874608+Burakyc@users.noreply.github.com>
2022-10-28 19:37:07 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+# donguler_while_foreach
+using System;
+
+namespace donguler_while_foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //While
+            // 1 den başlayarak console dan girilen sayıya kadar (sayı dahil) ortalama hesalayıp console a yazdıran program.
+            Console.Write("Lütfen bir sayi giriniz : ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac=1;
+            int toplam = 0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+                
+            }
+
+            Console.WriteLine(toplam/sayi);
+
+            // 'a' dan 'z' ye kadar tüm harfleri console a yadır.
+
+            char character = 'a';
+            while (character<'z')
+            {
+                Console.Write(character);
+                character ++;
+            }
+
+             Console.Write("********** Foreach *********");
+
+                string[] arabalar = {"Bmw","Ford","Toyata","Nissan"};
+
+             foreach (var araba in arabalar)
+             {
+                Console.WriteLine(araba);
+             }
+
+
+
+        }
+    }
+}
